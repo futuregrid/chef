ffda40d626367f4693e460807da813a14a80fbbe
Jonathan Klinginsmith <jklinginsmith@gmail.com>
2012-01-22 10:04:20 -0500

    Adding scripts for installing Chef on Centos 5.7 and getting started with Chef

---
+++ scripts/install_chef_centos_5.7_rbel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+########################################
+#
+# Installs Chef using the RBEL repo.
+#
+# This script has been tested on the following AMIs/EMIs in FutureGrid:
+#  * OpenStack  - ami-0000010d (centos57/centos57.manifest.xml)
+#  * Eucalyptus - emi-4E030DCC (centos57/centos57.manifest.xml)
+#
+########################################
+
+rpm -Uvh http://rbel.frameos.org/rbel5
+yum -y -d0 -e0 install rubygem-chef
